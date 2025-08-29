ffab14daacca99e5b31428db39b786a455f9ad54
austinxn <austinxn@gmail.com>
2025-08-28 18:59:30 -0700

    Add plotting script to python folder

---
+++ python/Plot_Vicsek.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("/locationhere/vicsek_output.csv", header=None, names=["t", "x", "y"])
+
+for t in range(0, 100, 1):
+    frame = df[df["t"] == t]
+    plt.clf()
+    plt.scatter(frame["x"], frame["y"], s=5)
+    plt.xlim(0, 10)
+    plt.ylim(0, 10)
+    plt.title(f"t = {t}")
+    plt.pause(0.01)
